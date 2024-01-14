b0b792baaf0bb0eba005781c2abecc133e8eadff
hushin <ngtv.hsn@gmail.com>
2024-01-14 20:55:05 +0900

    Add @astrojs/rss package and create rss.xml.js file

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET() {
+  return rss({
+    title: 'Astro学習者 | ブログ',
+    description: 'Astroを学ぶ旅',
+    site: 'https://main--creative-cassata-bd3e7e.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>ja-jp</language>`,
+  });
+}
